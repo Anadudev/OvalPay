c2bd1b3d3b516c01f55cda1ecd504876693b5ea4
Anadudev <anadudev@gmail.com>
2025-03-28 04:25:55 +0100

    Refactor WhyChooseUsCard component to improve background blur effect and enhance layout structure

--- src/components/WhyChooseUsCard.tsx
+++ src/components/WhyChooseUsCard.tsx
@@ -9,9 +9,9 @@ const WhyChooseUsCard: React.FC<WhyChooseUsCardProps> = ({
     <div className="min-h-[220px]">
       <div className="p-px rounded-[20px] flex-1 max-w-[626px]  bg-gradient-to-br from-secondary to-indigo-950">
         <div className="bg-background relative rounded-[20px] w-full h-full overflow-hidden">
-          <div className="absolute -top-10 -left-10 size-34 bg-radial from-indigo-500 to-background rounded-full"/>
-          <div className="absolute -bottom-10 -right-10 size-34 bg-radial from-indigo-500 to-background rounded-full"/>
-          <div className="bg-blur rounded-[20px] w-full h-full flex sm:flex-row flex-col items-center justify-center gap-6 p-9 sm:p-13">
+          <div className="absolute blur-3xl -top-10 -left-10 size-34 bg-radial  from-indigo-500 to-background rounded-full"/>
+          <div className="absolute blur-3xl -bottom-10 -right-10 size-34 bg-radial from-indigo-500 to-background rounded-full"/>
+          <div className="rounded-[20px] w-full h-full flex sm:flex-row flex-col items-center justify-center gap-6 p-9 sm:p-13">
             <div className="">
               <img src={image} alt="" className="size=[128px]" />
             </div>
